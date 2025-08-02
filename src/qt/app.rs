9b1bb46b0ff9ab0f1759798d956f3337ff2fc74d
Imanuel Ulbricht <me@imanuel.dev>
2025-08-03 01:30:09 +0200

    JWLS-4 Add the collect command

--- src/qt/app.rs
+++ src/qt/app.rs
@@ -5,14 +5,6 @@ cpp! {{
     #include <QtGui/QIcon>
 }}
 
-fn set_qapplication_icon(icon_path: &str) {
-    let icon_path = std::ffi::CString::new(icon_path).unwrap();
-    let icon_path = icon_path.as_ptr();
-    cpp!(unsafe [icon_path as "const char *"] {
-        QGuiApplication::setWindowIcon(QIcon(icon_path));
-    })
-}
-
 pub fn set_desktop_file(desktop_file: &str) {
     let desktop_file = std::ffi::CString::new(desktop_file).unwrap();
     let desktop_file = desktop_file.as_ptr();
